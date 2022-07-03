745ba214a9d0cc7a892f36c18509d117b848d66a
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-03 15:19:50 -0500

    Update mapper.py

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+import sys
+
+def clear_spaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    return x
+
+def purpose_amount(x):
+    return  clear_spaces(x[1]) +  "*" + clear_spaces(x[0])
+
+for line in sys.stdin:
+    result = line.split(',')
+    print(purpose_amount(result))
