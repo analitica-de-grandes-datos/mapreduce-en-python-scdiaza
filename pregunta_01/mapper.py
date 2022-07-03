6639c8af7a44be6b5899e60530bcd49e10a1bdf0
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-03 14:28:15 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+import sys
+
+if __name__ == "__main__":
+
+    for row in sys.stdin:
+        dividir = row.split(",") 
+        segundaCol = dividir[2]
+   
+        sys.stdout.write("{}\t1\n".format(segundaCol))
