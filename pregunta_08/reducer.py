fdbade4147420031f199342370c7d7772a39bb3f
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 02:20:50 -0500

    Update reducer.py

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,32 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    suma = None
+    promedio = None
+    count=0
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+            suma += val
+            count+= 1
+            promedio = suma/count
+        else:
+            if curkey is not None:
+                promedio=suma/count
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey,suma,promedio))
+                
+            curkey = key
+            suma = val
+            count=1
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey,suma,promedio))
+    
