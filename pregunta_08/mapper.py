3fe351ab0e7a2ed5a3a12b2e41a46e946f6d4a62
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 02:20:27 -0500

    Update mapper.py

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        limpiar = line.strip()
+        sys.stdout.write("{}\t{}\n".format(limpiar.split("  ")[0],limpiar.split("  ")[2]))
+        
