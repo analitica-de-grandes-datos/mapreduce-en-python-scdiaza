90bf8772fe18607d088fb5c99c26021bab83e513
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 01:55:41 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -4,15 +4,7 @@
 
 import sys
 
-biggest_purposes = {}
-
-def set_bigger_purpose(dictionary_purposes, actual_element):
-    element_array = actual_element.split("*")
-    dictionary_purposes[element_array[0]] = int(dictionary_purposes.get(actual_element[0]) or 0) + 1 
-    return dictionary_purposes
-
 for line in sys.stdin:
-    set_bigger_purpose(biggest_purposes, line)
-
-for purpose, amount in biggest_purposes.items():
-    print(purpose + "," + str(amount))
+    line = line.replace("\n","")
+    if '*' in line:
+        print(line.split("*")[1]+ "," +line.split("*")[0])
