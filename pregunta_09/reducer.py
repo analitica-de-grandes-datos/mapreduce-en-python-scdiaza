4cd4dc8bfaaec2939f986a6fc6a30b645e73825e
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 02:22:15 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+
+if __name__ == '__main__':
+
+    orden= [] 
+    for line in sys.stdin:
+
+        key, val1, val2 = line.strip().split(",")
+        val2 = int(val2)
+        orden.append([key,val1,val2])
+    
+    elements = sorted(orden, key =lambda x:(x[2]))
+    
+    for i in range(0,6):
+        sys.stdout.write("{}   {}   {}\n".format(elements[i][0],elements[i][1],elements[i][2]))
+        
