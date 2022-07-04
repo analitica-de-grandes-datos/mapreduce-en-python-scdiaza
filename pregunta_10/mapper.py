ca870a9121b016dd5261c0133a246d222f2f8b6b
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 02:23:17 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+
+        atributo, valor = line.split('\t')
+        valor = list(valor.split(','))
+
+        for letra in valor:
+            sys.stdout.write(
+                "{}-{}\n".format(letra.strip(), atributo.zfill(4)))
+            
