1655a06f40352ddb5664b3e38049cd647c00886c
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 02:14:15 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+if __name__ == '__main__':
+
+ campo_lista=[]
+ for line in sys.stdin:
+    marca, date, value = line.split("\t") 
+    value=int(value) 
+    campo_lista.append([marca, date, value]) 
+
+ campo_lista = sorted(campo_lista, key=lambda x: (x[0], x[2])) 
+ for line in campo_lista: 
+    sys.stdout.write(line[0] +"   " + line[1] +"   " + str(line[2]) + "\n")
+    
