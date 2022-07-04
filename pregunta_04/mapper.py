88fda38f169657113ea77646e805009c946f74e1
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 02:09:01 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,19 +1,19 @@
 #
-# >>> Escriba el codigo del reducer a partir de este punto <<<
+# >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 
 import sys
 
-biggest_purposes = {}
+def clear_spaces(x):
+    x = x.replace("\n", "")
+    x = x.replace("\r", "")
+    return x;
 
-def set_bigger_purpose(dictionary_purposes, actual_element):
-    element_array = actual_element.split("*")
-    dictionary_purposes[element_array[0]] = int(dictionary_purposes.get(actual_element[0]) or 0) + 1 
-    return dictionary_purposes
+def purpose_amount(x):
+    return clear_spaces(x[0])  + "*" + clear_spaces(x[2])
 
 for line in sys.stdin:
-    set_bigger_purpose(biggest_purposes, line)
-
-for purpose, amount in biggest_purposes.items():
-    print(purpose + "," + str(amount))
+    line = line.replace("'","")
+    result = line.split('   ')
+    print(purpose_amount(result))
     
