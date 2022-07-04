26581d60b7387c29cd5e6f52a05d8efbe4207341
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 02:23:37 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+
+if __name__ == '__main__':
+    anteratributo = None
+    total = 0
+
+    for line in sys.stdin:
+        atributo, valor = line.split("-")
+        valor = valor.strip()
+
+        if atributo == anteratributo:
+            numeros = numeros + ',' + str(int(valor))
+        else:
+            if anteratributo is not None:
+                sys.stdout.write("{}\t{}\n".format(anteratributo, numeros))
+            anteratributo = atributo
+            numeros = str(int(valor))
+
+    sys.stdout.write("{}\t{}\n".format(anteratributo, numeros))
+    
